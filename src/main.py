4ee489470d18b0d3045ba0bc5495794271b2a44a
shuaibahmed01 <121910026+shuaibahmed01@users.noreply.github.com>
2024-08-01 00:05:15 -0700

    more updates

--- src/main.py
+++ src/main.py
@@ -25,3 +25,33 @@ st.title("🤖 AI Dataframe Agent")
 # initialize chat history in streamlit session state
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
+
+# initiate df in session state
+if "df" not in st.session_state:
+    st.session_state.df = None
+
+# file upload widget
+uploaded_file = st.file_uploader("Choose a file", type=["csv", "xlsx", "xls"])
+
+if uploaded_file:
+    st.session_state.df = read_data(uploaded_file)
+    st.write("Dataframe Preview:")
+    st.dataframe(st.session_state.df.head())
+
+
+# display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# input field for users message
+user_prompt = st.chat_input("Ask Dataframe Agent...")
+
+if user_prompt:
+    # add user message to chat history and display it
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+    
+    # loading the LLM
+    llm = ChatOllama(model = "gemma:2b", temperature=0 )
